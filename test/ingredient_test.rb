849d52c09bc31e1cf30cf46bf10b252cd57c9ca7
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-03 10:26:41 -0600

    Add ingredient 2 to setup

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -6,6 +6,7 @@ require './lib/ingredient'
 class IngredientTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists
