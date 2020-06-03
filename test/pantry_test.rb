f3548e003b59d2e7cac84cd0c7e050ae60f68f2c
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-03 10:30:04 -0600

    Test pantry stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,4 +11,8 @@ class PantryTest < Minitest::Test
   def test_it_exists
     assert_instance_of Pantry, @pantry
   end
+
+  def test_pantry_stock
+    assert_equal Hash.new, @pantry.stock
+  end
 end
