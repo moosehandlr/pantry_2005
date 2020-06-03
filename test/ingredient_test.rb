8b81263f397bf2c0271b8d7306014922a354393d
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-03 10:18:22 -0600

    Test ingredient exists

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,5 +4,13 @@ require './lib/ingredient'
 
 
 class IngredientTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
+  def test_it_exists
+    assert_instance_of Ingredient, @ingredient
+  end
+
 
 end
