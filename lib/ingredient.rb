fcf1f55aec10c6cc74f275b0255b8ee3380ec1a5
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-03 10:23:39 -0600

    Test unit attribute

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient
+  attr_reader :name
   def initialize(ingredient_info)
     @name = ingredient_info[:name]
+    @unit = ingredient_info[:unit]
   end
 end
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -9,11 +9,13 @@ class IngredientTest < Minitest::Test
   end
 
   def test_it_exists
+    skip
     assert_instance_of Ingredient, @ingredient
   end
 
   def test_it_has_attributes
     assert_equal "Cheese", @ingredient1.name
+    assert_equal "oz", @ingredient1.unit
   end
 
 
